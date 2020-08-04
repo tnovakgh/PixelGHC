af6ac3b0908b67700b0c5da4113dec3b51f9a7d6
tnovakgh <novak_troy@yahoo.com>
2020-08-04 12:14:46 -0600

    Initial commit

---
+++ app/src/main/java/com/novak/android/pixelghc/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.novak.android.pixelghc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
